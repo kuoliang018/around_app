eb523ecf1fcdf9bb2ffab4507d8b053f7e905014
kuliang <kuliang@ucsd.edu>
2018-10-28 15:09:35 -0700

    using ant

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { TopBar } from './TopBar';
+import { Register } from './Register';
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-            <div className={"App-title"}>Around</div>
-        </header>
+        <TopBar/>
+        <Register/>
       </div>
     );
   }
