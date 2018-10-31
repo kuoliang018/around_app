63c7c166e66c079a621530e370e7783e34e28d17
kuliang <kuliang@ucsd.edu>
2018-10-30 20:07:58 -0700

    folder

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import logo from "./logo.svg"
+import logo from "../assets/images/logo.svg"
 export class TopBar extends React.Component {
     render() {
         return (
